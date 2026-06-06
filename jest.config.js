23b8e91135eb6b4ff7fc53a9e809762746a41686
SharefRRC <sislam48@academic.rrc.ca>
2026-06-05 19:03:42 -0500

    final commit with running server and passing tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"]
+  setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"]
 };
